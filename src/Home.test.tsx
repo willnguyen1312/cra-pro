91909eb8c6ca2ffec56019e4c88ae1ce381a0817
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-23 08:16:36 -0400

    Oh boy

--- src/Home.test.tsx
+++ src/Home.test.tsx
@@ -1,8 +1,79 @@
-import { render, screen } from "@testing-library/react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { PolarisTestProvider } from "@shopify/polaris";
+import "@testing-library/jest-dom";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { HttpResponse, graphql } from "msw";
+import { beforeEach } from "node:test";
 import { Home } from "./Home";
+import { server } from "./mocks/node";
 
-test("renders learn react link", () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const link = new HttpLink({
+  uri: "http://localhost:5173/graphql",
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
+function renderApp() {
+  render(
+    <ApolloProvider client={client}>
+      <PolarisTestProvider>
+        <Home />
+      </PolarisTestProvider>
+    </ApolloProvider>
+  );
+}
+
+beforeEach(() => {
+  server.resetHandlers();
+});
+
+test("Home component should render successfully on error case", async () => {
+  server.use(
+    graphql.query("ListMovies", () => {
+      return HttpResponse.json({
+        errors: [
+          {
+            message: `Cannot succeed!`,
+          },
+        ],
+      });
+    })
+  );
+
+  renderApp();
+
+  // Wait for the loading indicator to disappear
+  await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+
+  expect(screen.getByText("Error: Cannot succeed!")).toBeInTheDocument();
+});
+
+test("Home component should render successfully on happy case", async () => {
+  renderApp();
+
+  const user = userEvent.setup();
+  //   Wait for the loading indicator to disappear
+  await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+
+  expect(screen.getByText("The Matrix")).toBeInTheDocument();
+
+  expect(screen.getAllByRole("listitem").length).toBe(3);
+
+  const addNewMovieButton = screen.getByRole("button", { name: "Add Movie" });
+  await user.click(addNewMovieButton);
+
+  await screen.findByText("Total count: 4");
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -9,6 +9,40 @@ import "@shopify/react-testing/matchers";
 import "@testing-library/jest-dom";
 import { server } from "./mocks/node";
 
+import { TextDecoder, TextEncoder } from "node:util";
+
+import { Blob, File } from "node:buffer";
+import { FormData, Headers, Request, Response, fetch } from "undici";
+
+// Polyfill for msw
+// jest.polyfills.js
+/**
+ * @note The block below contains polyfills for Node.js globals
+ * required for Jest to function when running JSDOM tests.
+ * These HAVE to be require's and HAVE to be in this exact
+ * order, since "undici" depends on the "TextEncoder" global API.
+ *
+ * Consider migrating to a more modern test runner if
+ * you don't want to deal with this.
+ */
+
+Object.defineProperties(globalThis, {
+  TextDecoder: { value: TextDecoder },
+  TextEncoder: { value: TextEncoder },
+});
+
+Object.defineProperties(globalThis, {
+  fetch: { value: fetch, writable: true },
+  Blob: { value: Blob },
+  File: { value: File },
+  Headers: { value: Headers },
+  FormData: { value: FormData },
+  Request: { value: Request },
+  Response: { value: Response },
+});
+
+// Setup msw server
+
 beforeAll(() => {
   server.listen();
 });
